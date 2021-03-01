3fd250ac6cc10d3bfc07f9b5910541961e20a03d
wangyou <18911971839@163.com>
2021-03-01 09:56:00 +0800

    文字识别服务端接口调用控制逻辑

--- app/src/main/java/com/cloud/control/expand/service/utils/ConstantsUtils.java
+++ app/src/main/java/com/cloud/control/expand/service/utils/ConstantsUtils.java
@@ -10,4 +10,16 @@ public class ConstantsUtils {
     //图片中间拼接地址
     public static final String IMAGE_MIDDLE_URL = "api/upgrade/downlodFile/viewImage/?url=";
 
+    public interface OcrParams {
+        String MODEL_PATH_DEFAULT = "models/ocr_v1_for_cpu";
+        String LABEL_PATH_DEFAULT = "labels/ppocr_keys_v1.txt";
+        String IMAGE_PATH_DEFAULT = "images/5.jpg";
+        int CPU_THREAD_NUM_DEFAULT = 4;
+        String CPU_POWER_MODE_DEFAULT = "LITE_POWER_HIGH";
+        String INPUT_COLOR_FORMAT_DEFAULT = "BGR";
+        String INPUT_SHAPE_DEFAULT = "1,3,960";
+        String INPUT_MEAN_DEFAULT = "0.485, 0.456, 0.406";
+        String INPUT_STD_DEFAULT = "0.229,0.224,0.225";
+        String SCORE_THRESHOLD_DEFAULT = "0.1";
+    }
 }
